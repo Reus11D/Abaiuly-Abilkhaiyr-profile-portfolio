80d408683a603f51e6c01fc19bfde6b9f02bb1b6
Abaiuly <190103431@stu.sdu.edu.kz>
2021-03-16 15:09:54 +0600

    'post_test'

---
+++ tests/Feature/PostTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class PostTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $response = $this->get('/welcome');
+
+        $response->assertStatus(200);
+    }
+/////////////////////////////////////////////////////////////////////////////
+//Create funtions for lab 6
+    public function test_post_id_get_request()
+    {
+        $response = $this->get('/port/1');
+
+        $response->assertStatus(200);
+    }
+    public function test_get_unexisted_post()
+    {
+        $response = $this->get('/port/10000');
+
+        $response->assertStatus(404);
+    }
+    public function test_post_response()
+    {
+        $response = $this->get('/port/1');
+        $response->assertViewHas('port');
+       
+    }
+}
